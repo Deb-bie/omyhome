14c566ead9f7e3656d4f1a1731cdf5b9b98e22ae
Debbie <asamoahjaeldeborah@gmail.com>
2023-02-10 17:12:18 +0000

    Navbar

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -4,10 +4,7 @@ import {RxPerson} from "react-icons/rx";
 import {FaBars} from "react-icons/fa";
 import {MdClose} from "react-icons/md";
 import { BsArrowRight } from "react-icons/bs";
-
-
 import Mobile from "../../data/mobile.js"
-import Logo from "../../assets/logo/logo.svg";
 
 
 import "./style.css";
@@ -22,39 +19,39 @@ const Navbar = () => {
     const handleMobile = () => {setMobile(!mobile)}
 
   return (
-    <nav className='w-[100%] h-[100px] 2xl:h-[120px] sticky top-0 bg-[transparent] flex flex-col justify-between overflow-hidden z-[10] '>
-        <div className='w-[100%] h-[100%] flex flex-row justify-between '>
-            <section className='w-[75%] flex flex-row justify-start items-center text-xl pl-4 lg:pl-20 2xl:pl-32'>
-                <Link to="/">
-                    <div className=' '>
-                        <img src={Logo} alt="logo" className="w-[100%] h-[100%] object-contain " />
-                    </div>
+    <nav className='w-[100%] h-[100px] 2xl:h-[120px] sticky top-0 bg-[transparent] flex flex-row justify-center overflow-hidden z-[10] '>
+        <div className='w-[90%] h-[100%] flex flex-row justify-center'>
+            <div className='w-[100%] h-[100%] flex flex-row justify-between '>
+                <section className='w-[75%] flex flex-row justify-start items-center text-xl '>
+                    <Link to="/" className="font-['Moon_Dance'] text-[40px] md:text-[80px] ">
+                        omyHome
+                    </Link>
+
+                    <ul className='w-[65%] 2xl:w-[65%] hidden md:flex flex-row justify-between pl-12 lg:pl-20 2xl:pl-32'>
+                        <Link to="/buy-a-property" className='text-base lg:text-lg 2xl:text-2xl'>Buy</Link>
+                        <Link to="/rent-a-property" className='text-base lg:text-lg 2xl:text-2xl'>Rent</Link>
+                        <li>
+                            <span>
+                                <Link to="/agents" className='text-base lg:text-base 2xl:text-2xl'>Find Agents </Link>
+                            </span>
+                            /
+                            <span>
+                                <Link to="/home-owners" className='text-base lg:text-lg 2xl:text-2xl'> Home Owners</Link>
+                            </span>
+                        </li>
+                    </ul>
+                </section>
+
+                <Link to="/login" className='w-[25%] hidden md:flex flex-row justify-end items-center text-base lg:text-lg 2xl:text-2xl'>
+                    <RxPerson />
+                    &nbsp; &nbsp;
+                    Login
                 </Link>
 
-                <ul className='w-[65%] 2xl:w-[65%] hidden md:flex flex-row justify-between pl-12 lg:pl-20 2xl:pl-32'>
-                    <Link to="/buy-a-property" className='text-base lg:text-lg 2xl:text-2xl'>Buy</Link>
-                    <Link to="/rent-a-property" className='text-base lg:text-lg 2xl:text-2xl'>Rent</Link>
-                    <li>
-                        <span>
-                            <Link to="/agents" className='text-base lg:text-base 2xl:text-2xl'>Find Agents </Link>
-                        </span>
-                        /
-                        <span>
-                            <Link to="/home-owners" className='text-base lg:text-lg 2xl:text-2xl'> Home Owners</Link>
-                        </span>
-                    </li>
-                </ul>
-            </section>
-
-            <Link to="/login" className='w-[25%] hidden md:flex flex-row justify-end items-center text-base lg:text-lg 2xl:text-2live-serverxl md:pr-6 lg:pr-20 2xl:pr-32 '>
-                <RxPerson />
-                &nbsp; &nbsp;
-                Login
-            </Link>
-
-            <section onClick={() => handleMobile()} className='md:hidden flex flex-row items-center justify-center pr-4 text-3xl z-[80]'>
-                {mobile ? <MdClose /> : <FaBars />}
-            </section>
+                <section onClick={() => handleMobile()} className='md:hidden flex flex-row items-center justify-center pr-4 text-3xl z-[80]'>
+                    {mobile ? <MdClose /> : <FaBars />}
+                </section>
+            </div>
         </div>
 
         <hr className='bg-[#333] ' />
